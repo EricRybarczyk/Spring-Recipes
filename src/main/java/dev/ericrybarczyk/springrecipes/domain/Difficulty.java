61ca518a31d99a397a535eb2245421f240959ddd
EricRybarczyk <EricRybarczyk@gmail.com>
2020-08-01 11:43:55 -0400

    Added JPA Data Model Classes

---
+++ src/main/java/dev/ericrybarczyk/springrecipes/domain/Difficulty.java
@@ -0,0 +1,5 @@
+package dev.ericrybarczyk.springrecipes.domain;
+
+public enum Difficulty {
+    EASY, INTERMEDIATE, ADVANCED
+}
---
+++ src/main/java/dev/ericrybarczyk/springrecipes/domain/Ingredient.java
@@ -0,0 +1,59 @@
+package dev.ericrybarczyk.springrecipes.domain;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private BigDecimal quantity;
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure unitOfMeasure;
+    @ManyToOne
+    private Recipe recipe;
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(BigDecimal quantity) {
+        this.quantity = quantity;
+    }
+
+    public UnitOfMeasure getUnitOfMeasure() {
+        return unitOfMeasure;
+    }
+
+    public void setUnitOfMeasure(UnitOfMeasure unitOfMeasure) {
+        this.unitOfMeasure = unitOfMeasure;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+}
