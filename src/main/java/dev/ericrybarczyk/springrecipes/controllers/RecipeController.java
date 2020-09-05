f80057e1fc1e38017ea659fe97f441a69b60ca71
EricRybarczyk <EricRybarczyk@gmail.com>
2020-09-05 19:43:35 -0400

    Added example of Bean Validation with Hibernate Validators
    RecipeCommand validation attributes and usage with BindingResult in RecipeController

--- src/main/java/dev/ericrybarczyk/springrecipes/controllers/RecipeController.java
+++ src/main/java/dev/ericrybarczyk/springrecipes/controllers/RecipeController.java
@@ -6,10 +6,12 @@ import dev.ericrybarczyk.springrecipes.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import javax.validation.Valid;
 import java.util.Optional;
 
 
@@ -19,6 +21,9 @@ public class RecipeController {
 
     private final RecipeService recipeService;
 
+    private static final String  RECIPE_SHOW_VIEW_NAME = "recipe/show";
+    private static final String  RECIPE_FORM_VIEW_NAME = "recipe/recipeform";
+
     public RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
     }
@@ -27,13 +32,13 @@ public class RecipeController {
     public String showById(@PathVariable String id, Model model) {
         Optional<Recipe> optionalRecipe = recipeService.findById(Long.valueOf(id));
         optionalRecipe.ifPresent(recipe -> model.addAttribute("recipe", recipe));
-        return "recipe/show";
+        return RECIPE_SHOW_VIEW_NAME;
     }
 
     @GetMapping("recipe/new")
     public String newRecipe(Model model) {
         model.addAttribute("recipe", new RecipeCommand());
-        return "recipe/recipeform";
+        return RECIPE_FORM_VIEW_NAME;
     }
 
     @GetMapping("recipe/{id}/edit")
@@ -42,11 +47,15 @@ public class RecipeController {
         if (recipeCommand != null) {
             model.addAttribute("recipe", recipeCommand);
         }
-        return "recipe/recipeform";
+        return RECIPE_FORM_VIEW_NAME;
     }
 
     @PostMapping("recipe")
-    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+    public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand command, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            bindingResult.getAllErrors().forEach(e -> log.debug(e.toString()));
+            return RECIPE_FORM_VIEW_NAME;
+        }
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
         return String.format("redirect:/recipe/%s/show", savedCommand.getId());
     }
--- src/test/java/dev/ericrybarczyk/springrecipes/controllers/RecipeControllerTest.java
+++ src/test/java/dev/ericrybarczyk/springrecipes/controllers/RecipeControllerTest.java
@@ -88,9 +88,28 @@ public class RecipeControllerTest {
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .param("id", "")
                 .param("description", "some string value")
+                .param("directions", "some string value")
         )
                 .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
                 .andExpect(MockMvcResultMatchers.view().name("redirect:/recipe/2/show"));
+    }
+
+    @Test
+    public void testPostNewRecipeValidationFail() throws Exception {
+        // given
+        RecipeCommand recipeCommand = new RecipeCommand();
+        recipeCommand.setId(2L);
+        Mockito.when(recipeService.saveRecipeCommand(ArgumentMatchers.any(RecipeCommand.class))).thenReturn(recipeCommand);
+
+        // when-then
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/recipe")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("id", "")
+        )
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.model().attributeExists("recipe"))
+                .andExpect(MockMvcResultMatchers.view().name("recipe/recipeform"));
     }
 
     @Test
